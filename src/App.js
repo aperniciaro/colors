1d32519c2951385ac6f0b626c8537d7fbe4331e0
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-21 16:22:41 -0500

    added saving of colors and displaying saved list

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ class App extends Component {
   state = {
     hue: 50,
     saturation: 50,
-    lightness: 50
+    lightness: 50,
+    savedColors: []
   }
 
   changeHue = event => {
@@ -19,24 +20,16 @@ class App extends Component {
     this.setState({ lightness: event.target.value })
   }
 
-  // saveColor = event => {
-  //   const myHue = this.state.hue
-  //   const mySaturation = this.state.saturation
-  //   const myLightness = this.state.lightness
-  //   return (
-  //     <section className="saved-color">
-  //       <section
-  //         className="swatch"
-  //         style={{
-  //           backgroundColor: `hsl(${myHue},${mySaturation}%,${myLightness}%)`
-  //         }}
-  //       />
-  //       <p className="color-values">
-  //         H: {myhue}, S: {mySaturation}, L: {myLightness}
-  //       </p>
-  //     </section>
-  //   )
-  // }
+  saveColor = event => {
+    const color = {
+      myHue: this.state.hue,
+      mySaturation: this.state.saturation,
+      myLightness: this.state.lightness
+    }
+    this.setState({
+      savedColors: this.state.savedColors.concat(color)
+    })
+  }
 
   render() {
     return (
@@ -65,14 +58,32 @@ class App extends Component {
           <input type="range" onChange={this.changeSaturation} />
           <h3 className="slider-label">Lightness</h3>
           <input type="range" onChange={this.changeLightness} />
-          <button className="save-color" onclick={this.saveColor}>
+          <button className="save-color" onClick={this.saveColor}>
             Save Color
           </button>
         </section>
         <section className="saved-list">
-          <section className="saved-color">
-            <h2 className="saved-list-header">Your Colors: </h2>
-          </section>
+          <h2 className="saved-list-header">Your Colors: </h2>
+          <ul>
+            {this.state.savedColors.map(color => {
+              return (
+                <li class="saved-color">
+                  <section
+                    className="swatch"
+                    style={{
+                      backgroundColor: `hsl(${color.myHue},${
+                        color.mySaturation
+                      }%,${color.myLightness}%)`
+                    }}
+                  />
+                  <p className="color-values">
+                    H: {color.myHue}, S: {color.mySaturation}%, L:{' '}
+                    {color.myLightness}%
+                  </p>
+                </li>
+              )
+            })}
+          </ul>
         </section>
       </>
     )
