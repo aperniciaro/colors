c49af805c6a8e393f96ddc1b43ea08833c8cde7a
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-21 14:09:17 -0500

    dynamically fill main color data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 
 class Header extends Component {
+  state = {
+    hue: 50,
+    saturation: 50,
+    lightness: 50
+  }
+
   render() {
     return (
       <header>
         <h1 className="title">Color Selector</h1>
         <section className="active-color">
-          <section className="swatch" />
-          <p className="color-values">H: , S: , L: </p>
+          <section
+            className="swatch"
+            style={{
+              backgroundColor: `hsl(${this.state.hue},${
+                this.state.saturation
+              }%,${this.state.lightness}%)`
+            }}
+          />
+          <p className="color-values">
+            H: {this.state.hue}, S: {this.state.saturation}%, L:{' '}
+            {this.state.lightness}%
+          </p>
         </section>
       </header>
     )
