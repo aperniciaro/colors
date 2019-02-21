6bd3709fe072be0a1c0308b219390b7a36ab2d1e
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-21 15:03:59 -0500

    removed excess html

--- src/App.js
+++ src/App.js
@@ -72,10 +72,6 @@ class App extends Component {
         <section className="saved-list">
           <section className="saved-color">
             <h2 className="saved-list-header">Your Colors: </h2>
-            <section className="saved-color">
-              <section className="swatch" />
-              <p className="color-values">H: , S: , L: </p>
-            </section>
           </section>
         </section>
       </>
