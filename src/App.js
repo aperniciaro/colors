4ef76a2d82f5a7ed7ff129953132ec60d58aecf8
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-21 13:39:40 -0500

    general layout and html

--- src/App.js
+++ src/App.js
@@ -3,7 +3,33 @@ import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <h1 className="page-header">Color Selector</h1>
+        <section className="color-display">
+          <section className="color-sample">
+            <section className="swatch" />
+            <p className="color-values">H: , S: , L: </p>
+          </section>
+        </section>
+        <section className="controls">
+          <h3 className="slider-label">Hue</h3>
+          <input type="range" />
+          <h3 className="slider-label">Saturation</h3>
+          <input type="range" />
+          <h3 className="slider-label">Lightness</h3>
+          <input type="range" />
+          <button className="save-color">Save Color</button>
+        </section>
+        <section className="saved-list">
+          <h2 classname="saved-list-header">Your Colors: </h2>
+          <section className="color-sample">
+            <section className="swatch" />
+            <p className="color-values">H: , S: , L: </p>
+          </section>
+        </section>
+      </>
+    )
   }
 }
 
