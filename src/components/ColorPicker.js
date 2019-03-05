f751b54957b95b31cd98b998f309bf3f9e5a9bfa
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-05 12:36:30 -0500

    changed color sliders to use hooks; need to restore save fucntionality

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,65 +1,59 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function ColorPicker() {
-  const [hue, setHue] = useState(0)
-  const [saturation, setSaturation] = useState(0)
-  const [lightness, setLightness] = useState(0)
+  const [hue, setHue] = useState(50)
+  const [saturation, setSaturation] = useState(50)
+  const [lightness, setLightness] = useState(50)
 
-  changeHue = event => {
-    this.setState({ hue: event.target.value })
-  }
+  // saveColor = event => {
+  //   const color = {
+  //     myHue: this.state.hue,
+  //     mySaturation: this.state.saturation,
+  //     myLightness: this.state.lightness
+  //   }
+  //   this.setState({
+  //     savedColors: this.state.savedColors.concat(color)
+  //   })
+  // }
 
-  changeSaturation = event => {
-    this.setState({ saturation: event.target.value })
-  }
-
-  changeLightness = event => {
-    this.setState({ lightness: event.target.value })
-  }
-
-  saveColor = event => {
-    const color = {
-      myHue: this.state.hue,
-      mySaturation: this.state.saturation,
-      myLightness: this.state.lightness
-    }
-    this.setState({
-      savedColors: this.state.savedColors.concat(color)
-    })
-  }
-
-  render() {
-    return (
-      <>
-        <header>
-          <h1 className="title">Color Selector</h1>
-          <section className="active-color">
-            <section
-              className="swatch"
-              style={{
-                backgroundColor: `hsl(${this.state.hue},${
-                  this.state.saturation
-                }%,${this.state.lightness}%)`
-              }}
-            />
-            <p className="color-values">
-              H: {this.state.hue}, S: {this.state.saturation}%, L:{' '}
-              {this.state.lightness}%
-            </p>
-          </section>
-        </header>
-        <section className="controls">
-          <h3 className="slider-label">Hue</h3>
-          <input type="range" onChange={this.changeHue} max="240" />
-          <h3 className="slider-label">Saturation</h3>
-          <input type="range" onChange={this.changeSaturation} />
-          <h3 className="slider-label">Lightness</h3>
-          <input type="range" onChange={this.changeLightness} />
-          <button className="save-color" onClick={this.saveColor}>
-            Save Color
-          </button>
+  return (
+    <>
+      <header>
+        <h1 className="title">Color Selector</h1>
+        <section className="active-color">
+          <section
+            className="swatch"
+            style={{
+              backgroundColor: `hsl(${hue},${saturation}%,${lightness}%)`
+            }}
+          />
+          <p className="color-values">
+            H: {hue}, S: {saturation}%, L: {lightness}%
+          </p>
         </section>
-        <section className="saved-list">
+      </header>
+      <section className="controls">
+        <h3 className="slider-label">Hue</h3>
+        <input
+          type="range"
+          onChange={event => setHue(event.target.value)}
+          max="240"
+        />
+        <h3 className="slider-label">Saturation</h3>
+        <input
+          type="range"
+          onChange={event => setSaturation(event.target.value)}
+        />
+        <h3 className="slider-label">Lightness</h3>
+        <input
+          type="range"
+          onChange={event => setLightness(event.target.value)}
+        />
+        {/* <button className="save-color" onClick={this.saveColor}>
+            Save Color
+          </button> */}
+      </section>
+      {/* <section className="saved-list">
           <h2 className="saved-list-header">Your Colors: </h2>
           <ul>
             {this.state.savedColors.map(color => {
@@ -81,7 +75,7 @@ export default function ColorPicker() {
               )
             })}
           </ul>
-        </section>
-      </>
-  }
+        </section> */}
+    </>
+  )
 }
